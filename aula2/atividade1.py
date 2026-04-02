87311b91c7ba1790a6b1062c569a7b6b4d7ee64a
victorcruz23 <victor.cruz23@gmail.com>
2026-04-02 20:57:21 -0300

    adiciona os arquivos da aula 02

---
+++ aula2/atividade1.py
@@ -0,0 +1,27 @@
+# # print ('hello world')
+
+# # #--------------------
+
+# numero = int(input(f'Digite o Numero: '))
+
+# numero_anterior = numero - 1
+# numero_posterior = numero + 1
+
+# print (f'O numero digitado foi: {numero}')
+# print (f'O numero anterior é igual: {numero_anterior}')
+# print (f'O numero posterior é igual: {numero_posterior}')
+
+# salario = float(input('Informe o salario R$: '))
+
+# acrescimo = salario * 0.15
+
+# salario_total = salario + acrescimo
+
+# print (f'Ocorreu um acréscimo de 15% (R$:{acrescimo:.2f}) do colaborador XPTO neste mês, desta forma, o Valor total ficou sendo de R$: {salario_total:.2f} ')
+
+print ('hello world')
+
+
+
+
+
---
+++ aula2/exemplo_02.py
@@ -0,0 +1,104 @@
+# print ("hello world")
+
+# #var 
+
+# n1 = 5
+# n2 = 2
+
+
+# soma = n1 + n2
+
+# print (f'Total: {soma}')
+
+# nome = "joao"
+# idade = 20
+
+# print (f'Hello {nome}')
+# print (f'You have {idade} years')
+
+# # -------------------------------------
+
+# # exercicio de fixacao
+
+# nome = "alex"
+# bairro = "centro"
+# profissao = "professor"
+
+# print (f'O nome do candidato é {nome}, que tem como profissao {profissao} e reside no bairro {bairro}')
+
+# print ("brasil")
+
+# #-----------------------
+
+# #operadores
+
+# preco = 20
+# quantidade = 3
+
+# total = preco * quantidade
+
+# print (total)
+
+# #--------------
+
+# salario = 4000
+# bonus = 880
+
+# total = salario + bonus
+
+# print (f'O total do salário acrescido do bônus é de R$: {total}')
+
+#--------------
+
+# Exemplo de entrada de valores pelo user
+
+# nome = input ("Informe o Nome: ")
+# print (f"Olá, {nome}")
+
+
+# salario = float(input('Informe o Salario: '))
+# bonus = float(input('Informe o Bonus: '))
+
+# total = salario + bonus
+
+# print (f'O total do salario acrescido do bonus é de {total}')
+
+
+#-------------------
+
+
+nome = input ("Informe o Nome: ")
+print (f"Olá, {nome}")
+
+
+salario = float(input('Informe o Salario: '))
+bonus = float(input('Informe o Bonus: '))
+
+total = salario + bonus
+
+print (f'O total do salario acrescido do bonus é de {total}')
+
+# string = texto
+# 
+
+#----------------------------
+
+# produto
+# preco
+# quantidade comprada
+# valor total + 10%
+# quanto tenho que pagar o desconto
+
+# Calculo de %
+
+Nome_Produto = input('Informe o Produto: ')
+preco_produto = float(input(f'Informe o Preço: '))
+Quantidade_Comprada = int(input(f'Informe a Quantidade: '))
+Valor_total = preco_produto * Quantidade_Comprada
+Desconto = Valor_total * 0.1
+
+Total_Pagamento = Valor_total - Desconto
+
+print (f'O valor total de Pagamento é R$: {Total_Pagamento}, com desconto de R$: {Desconto}')
+
+ 
