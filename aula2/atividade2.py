f0c42e8a6be23186bd6045f53ed1899b729da437
victorcruz23 <victor.cruz23@gmail.com>
2026-04-02 21:41:44 -0300

    inclui a atividade 02 na pasta correta

---
+++ aula2/atividade2.py
@@ -0,0 +1,9 @@
+# print ('hello world')
+
+numero = int(input(f'Digite o Numero: '))
+
+dobro = numero * 2
+triplo = numero * 3
+quadrado = numero ** 2
+
+print (f'Para o numero informado {numero}, o dobro de seu valor é {dobro}, o triplo é {triplo} e ao quadrado é {quadrado}')
