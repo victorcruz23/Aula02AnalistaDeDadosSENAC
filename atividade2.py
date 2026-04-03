653da9c4b5a082b9a802e7269317ff6942ae8cff
victorcruz23 <victor.cruz23@gmail.com>
2026-04-02 21:50:07 -0300

    deleta o arquivo anterior atividade 2, que estava fora da pasta

--- atividade2.py
+++
@@ -1,9 +0,0 @@
-# print ('hello world')
-
-numero = int(input(f'Digite o Numero: '))
-
-dobro = numero * 2
-triplo = numero * 3
-quadrado = numero ** 2
-
-print (f'Para o numero informado {numero}, o dobro de seu valor é {dobro}, o triplo é {triplo} e ao quadrado é {quadrado}')
